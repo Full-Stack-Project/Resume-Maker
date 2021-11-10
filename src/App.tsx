067ae94e28eb5f59b8059f415c2121d423df9c75
Utkarshg25 <utkarsh.gangwar_cs19@gla.ac.in>
2021-11-10 17:52:23 +0530

    image file upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import Images from "./Images";
 function App() {
   return (
-    <div className="font-extrabold text-5xl bg-black text-white">
-      This is Resume Maker Project
-    </div>
+    // <div className="font-extrabold text-5xl bg-black text-white">
+    //   This is Resume Maker Project
+    // </div>
+    <Images />
   );
 }
 
